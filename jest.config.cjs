b00b7a592934958cab0a08c23310e09dc1bfd516
shatwik7 <shatwikpandey9706@gmail.com>
2026-04-19 12:49:35 +0000

    test(reports): test execution report and code coverage report will be genrated

--- jest.config.cjs
+++ jest.config.cjs
@@ -11,6 +11,21 @@ module.exports = {
       },
     ],
   },
+  coverageDirectory: "coverage",
+  coverageReporters: ["text-summary", "lcov", "cobertura", "html"],
   collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts"],
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "reports/junit",
+        outputName: "junit.xml",
+        suiteNameTemplate: "{filepath}",
+        classNameTemplate: "{classname}",
+        titleTemplate: "{title}",
+      },
+    ],
+  ],
   moduleFileExtensions: ["ts", "js", "json"],
 };
